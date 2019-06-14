6b6b511445c642b1248686dd747ed5ae1afbacfc
cc8080 <ccallas2000@yahoo.com>
2019-06-13 18:04:38 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -12,11 +12,13 @@ function onContextMenuClick(info, tab) {
 };
 
 /**
- * Create a context menu 
+ * Create a context menu. Match all allowed URLs so that can
+ * work with iframes that have a "" src attribute.
  */
 chrome.contextMenus.create({
   "title" : "Frame-Fit: adjust frame height",
   "type" : "normal",
   "contexts" : ["page", "frame"],
+  "documentUrlPatterns": ["<all_urls>"],
   "onclick" : onContextMenuClick
 });
